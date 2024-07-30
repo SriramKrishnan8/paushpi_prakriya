153d688033c433d9d7034cf5e99868fc2137f319
Simran Singh <m.simx2909@gmail.com>
2024-07-30 15:52:15 +0530

    refactored arg parse logic in line 296 to 315

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 import re
-
+import pytest
 import argparse
 
 from saFjFA.iw_saFjFA import print_iw
@@ -18,85 +18,85 @@ from upAfga.vikaraNa import insert_vikaraNa
 def test_iw():
     """ For internal testing
     """
-    
+
     iw_examples = [
-        "suz", "Bavawuz", "gamLz", "dupacazR", "BU", "tuozSvi", "lazN", 
-        "vaxiz", "dukqF", "kwvA", "wumun", "Sap", "NIF", "Bixir", "Cixizr", 
-        "FimixAz", "tunaxIz", "RAkan", "RPak", "RTac", "RPa", "jas", "tA", 
-        "Sap", "Syan", "gsnu", "Kyun", "Snam", "Sawq", "SAnac", "lyut", 
-        "kyac", "KiRNuc", "fi", "GaF", "wasil", "lac", "gmini", "Gan", "Sas", 
-        "ka", "Ka", "Namul", "uzCqxizr", "Ihaz", "uzXrasaz", "ozpyAyoz", 
+        "suz", "Bavawuz", "gamLz", "dupacazR", "BU", "tuozSvi", "lazN",
+        "vaxiz", "dukqF", "kwvA", "wumun", "Sap", "NIF", "Bixir", "Cixizr",
+        "FimixAz", "tunaxIz", "RAkan", "RPak", "RTac", "RPa", "jas", "tA",
+        "Sap", "Syan", "gsnu", "Kyun", "Snam", "Sawq", "SAnac", "lyut",
+        "kyac", "KiRNuc", "fi", "GaF", "wasil", "lac", "gmini", "Gan", "Sas",
+        "ka", "Ka", "Namul", "uzCqxizr", "Ihaz", "uzXrasaz", "ozpyAyoz",
         "ozladiz", "ozvrascUz", "tuozsPUrjAz"
     ]
-    
+
     for iw_ex in iw_examples:
         print_iw(iw_ex)
-    
+
 
 def test_prawyAhAra():
     """ All available prawyAhAras are enlisted here
     """
-    
+
     from util import all_prawyAhAras_list
-    
+
     for pr in all_prawyAhAras_list:
         print_prawyAhAra(pr)
-        
+
 
 def print_ti(term):
     """ """
-    
+
     ti_term, ti_start_index = ti(term)
     print(term + " -> " + str(ti_term) + ": " + str(ti_start_index))
 
 
 def test_ti():
     """ """
-    
+
     ti_examples = [
-        "suz", "Bavawuz", "gamLz", "dupacazR", "BU", "tuozSvi", "lazN", 
-        "vaxiz", "dukqF", "kwvA", "wumun", "Sap", "NIF", "Bixir", "Cixizr", 
-        "FimixAz", "tunaxIz", "RAkan", "RPak", "RTac", "RPa", "jas", "tA", 
+        "suz", "Bavawuz", "gamLz", "dupacazR", "BU", "tuozSvi", "lazN",
+        "vaxiz", "dukqF", "kwvA", "wumun", "Sap", "NIF", "Bixir", "Cixizr",
+        "FimixAz", "tunaxIz", "RAkan", "RPak", "RTac", "RPa", "jas", "tA",
         "Sap",
     ]
-    
+
     for pr in ti_examples:
         print_ti(pr)
 
 
 def print_upaXA(term):
     """ """
-    
+
     from saFjFA.saFjFA import upaXA
-    
+
     upaXA_str, upaXA_ind, upaXA_type = upaXA(term)
     print(term + " -> " + upaXA_str + "," + str(upaXA_ind) + "," + upaXA_type)
 
 
 def test_upaXA():
     """ """
-    
+
     examples = [
-        "suz", "Bavawuz", "gamLz", "dupacazR", "BU", "tuozSvi", "lazN", 
-        "vaxiz", "dukqF", "kwvA", "wumun", "Sap", "NIF", "Bixir", "Cixizr", 
-        "FimixAz", "tunaxIz", "RAkan", "RPak", "RTac", "RPa", "jas", "tA", 
+        "suz", "Bavawuz", "gamLz", "dupacazR", "BU", "tuozSvi", "lazN",
+        "vaxiz", "dukqF", "kwvA", "wumun", "Sap", "NIF", "Bixir", "Cixizr",
+        "FimixAz", "tunaxIz", "RAkan", "RPak", "RTac", "RPa", "jas", "tA",
         "Sap",
     ]
-    
+
     for pr in examples:
         print_upaXA(pr)
-        
+
 
 def test_sandhi(first, second):
     """ """
-    
+
     res = sanXi_op(first, second)
     print(first + " + " + second + " -> " + res)
 
 
 def test_sandhi_examples():
     """ """
-    
+
     examples_ac = [
         ("prawi", "ekaH"), ("maXu", "ariH"), ("XAwq", "aMSaH"), ("L", "AkqwiH"),
         ("ne", "a"), ("Bo", "a"), ("XyE", "a"), ("pO", "a"),
@@ -104,32 +104,32 @@ def test_sandhi_examples():
         ("na", "api"), ("Bava", "Ani"), ("ramA", "aswi"),
         ("muni", "ISaH"), ("paTawi", "ixam"), ("gOrI", "iyam"),
         ("laGu", "UrmiH"), ("BAnu", "uxayaH"), ("SvaSrU", "UkAraH"),
-        ("howq", "qkAraH"), 
+        ("howq", "qkAraH"),
         ("sura", "ISaH"), ("waWA", "iwi"), ("sUrya", "uxayaH"), ("gafgA", "uxakam"),
         ("mahA", "qRiH"), ("wava", "LkAraH"),
         ("mahA", "ORaXam"), ("gafgA", "oGaH"), ("kqRNa", "ekawvam"), ("eXa", "E"),
-        ("naya", "mi"), ("naya", "vaH"), ("anaya", "va"), ("anaya", "ma"), 
-        ("anaya", "vahi"), ("anaya", "mahi"), 
-        ("viSNU", "imO"), ("gafge", "amU"), ("harI", "ewO"), 
-        ("rAma", "am"), ("muni", "am"), ("guru", "am"), 
-        ("hare", "ava"), ("viSNo", "ava"), 
+        ("naya", "mi"), ("naya", "vaH"), ("anaya", "va"), ("anaya", "ma"),
+        ("anaya", "vahi"), ("anaya", "mahi"),
+        ("viSNU", "imO"), ("gafge", "amU"), ("harI", "ewO"),
+        ("rAma", "am"), ("muni", "am"), ("guru", "am"),
+        ("hare", "ava"), ("viSNo", "ava"),
     ]
-    
+
     examples_hal = [
-        ("haris", "Sewe"), ("xus", "cariwaH"), ("saw", "ciw"), ("uw", "cAraNam"), 
-        ("ux", "jvalaH"), ("sax", "janaH"), ("yAc", "nA"), ("SArfgin", "jayaH"), 
-        ("lolot", "wi"), ("xuR", "taH"), ("waw", "tIkA"), ("peR", "wA"), 
-        ("rAmas", "RaRTaH"), ("viR", "nuH"), ("ux", "dInaH"), ("kqR", "naH"), 
-        ("ne", "syawi"), ("ho", "syawi"), ("Sak", "syawi"), ("pA", "syawi"), 
-        ("aSASak", ""), ("acAkaK", ""), ("arArag", ""), ("ajAjaG", ""), 
-        ("arArat", ""), ("aloloT", ""), ("acAkad", ""), ("avAvaD", ""), 
-        ("ayAyaw", ""), ("avAvyaW", ""), ("arArax", ""), ("arAraX", ""), 
-        ("alAlap", ""), ("arAraP", ""), ("acAkab", ""), ("alAlaB", ""), 
-        ("acAkAS", ""), ("alAlaR", ""), 
-        ("xuG", ""), ("boX", "sya"), ("buX", "Xvam"), 
-        ("aBiws", "wa"), 
+        ("haris", "Sewe"), ("xus", "cariwaH"), ("saw", "ciw"), ("uw", "cAraNam"),
+        ("ux", "jvalaH"), ("sax", "janaH"), ("yAc", "nA"), ("SArfgin", "jayaH"),
+        ("lolot", "wi"), ("xuR", "taH"), ("waw", "tIkA"), ("peR", "wA"),
+        ("rAmas", "RaRTaH"), ("viR", "nuH"), ("ux", "dInaH"), ("kqR", "naH"),
+        ("ne", "syawi"), ("ho", "syawi"), ("Sak", "syawi"), ("pA", "syawi"),
+        ("aSASak", ""), ("acAkaK", ""), ("arArag", ""), ("ajAjaG", ""),
+        ("arArat", ""), ("aloloT", ""), ("acAkad", ""), ("avAvaD", ""),
+        ("ayAyaw", ""), ("avAvyaW", ""), ("arArax", ""), ("arAraX", ""),
+        ("alAlap", ""), ("arAraP", ""), ("acAkab", ""), ("alAlaB", ""),
+        ("acAkAS", ""), ("alAlaR", ""),
+        ("xuG", ""), ("boX", "sya"), ("buX", "Xvam"),
+        ("aBiws", "wa"),
     ]
-    
+
     for first, second in examples_hal:
         res = sanXi_op(first, second)
         print(first + " + " + second + " -> " + ",".join(res))
@@ -137,7 +137,7 @@ def test_sandhi_examples():
 
 def print_XAwu_changes(XAwu):
     """ """
-    
+
     print("\nmUla_XAwu: " + XAwu)
     raw_XAwu = XAwu_operations(XAwu)
     print("XAwu: " + raw_XAwu)
@@ -145,31 +145,31 @@ def print_XAwu_changes(XAwu):
 
 def test_XAwu_changes():
     """ """
-    
+
     from util import XAwus_list
-    
+
     for XAwu in XAwus_list:
         print_XAwu_changes(XAwu)
 
 
 def print_paxa(XAwu):
     """ """
-    
+
     paxa = paxanirNaya_viXi(XAwu)
     print(XAwu + ": " + paxa)
 
 
 def test_paxa():
     """ """
-    
-    temp_Xawus_list = [ "pac", "yAc", "eX", "As", "paT", "gam" ]
+
+    temp_Xawus_list = ["pac", "yAc", "eX", "As", "paT", "gam"]
 
     for XAwu in temp_Xawus_list:
         print_paxa(XAwu)
 
 
 def print_vikaraNa(upasarga, XAwu, prawyaya_type, prayoga, gaNa):
-    """ """ 
+    """ """
 
     res_lst = insert_vikaraNa(upasarga, XAwu, prawyaya_type, prayoga, gaNa)
     upasarga = upasarga or "-"
@@ -179,8 +179,8 @@ def print_vikaraNa(upasarga, XAwu, prawyaya_type, prayoga, gaNa):
 
 def test_vikaraNa():
     """ """
-    
-    temp_input_list = [ 
+
+    temp_input_list = [
         ("", "pac", "sArvaXAwuka", "karwari", "BvAxi"),
         ("", "pac", "sArvaXAwuka", "karmaNi", "BvAxi"),
         ("", "pac", "sArvaXAwuka", "BAve", "BvAxi"),
@@ -209,12 +209,12 @@ def test_vikaraNa():
 
 
 def print_prakriyA(upasarga, XAwu, gaNa, prayoga, lakAra, paxa):
-    """ """ 
+    """ """
 
     from prakriyA.prakriyA import generate
 
     res = generate(upasarga, XAwu, gaNa, prayoga, lakAra, paxa)
-    
+
     for e in res:
         p_e = []
         for i in e:
@@ -229,7 +229,7 @@ def print_prakriyA(upasarga, XAwu, gaNa, prayoga, lakAra, paxa):
 
 
 def test_prakriyA():
-    """ """ 
+    """ """
 
     examples_lat = [
         ("", "BU", "BvAxi", "karwari", "lat", "parasmEpaxa"),
@@ -268,10 +268,10 @@ def test_prakriyA():
 
 def main():
     """ """
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-t", "--type", default="iw", 
+        "-t", "--type", default="iw",
         choices=[
             "iw", "prawyAhAra", "ti", "upaXA", "sanXi",
             "XAwu_changes", "paxa", "vikaraNa", "prakriyA"
@@ -293,46 +293,26 @@ def main():
 
     args = parser.parse_args()
 
-    if args.type == "iw":
-        if args.own:
-            print_iw(args.own)
-        else:
-            test_iw()
-    elif args.type == "prawyAhAra":
-        if args.own:
-            print_prawyAhAra(args.own)
-        else:
-            test_prawyAhAra()
-    elif args.type == "ti":
-        if args.own:
-            print_ti(args.own)
-        else:
-            test_ti()
-    elif args.type == "upaXA":
-        if args.own:
-            print_upaXA(args.own)
-        else:
-            test_upaXA()
-    elif args.type == "sanXi":
-        if args.first or args.second:
+    function_mapping = {
+        "iw": (print_iw, test_iw),
+        "prawyAhAra": (print_prawyAhAra, test_prawyAhAra),
+        "ti": (print_ti, test_ti),
+        "upaXA": (print_upaXA, test_upaXA),
+        "XAwu_changes": (print_XAwu_changes, test_XAwu_changes),
+        "paxa": (print_paxa, test_paxa),
+        "vikaraNa": (None, test_vikaraNa),
+        "prakriyA": (None, test_prakriyA),
+        "sanXi": (None, test_sandhi_examples)
+    }
+    if args.type in function_mapping:
+        if args.type == 'sanXi' and (args.first and args.second):
             test_sandhi(args.first, args.second)
-            # print("Please provide the two terms to be sandhied")
         else:
-            test_sandhi_examples()
-    elif args.type == "XAwu_changes":
-        if args.own:
-            print_XAwu_changes(args.own)
-        else:
-            test_XAwu_changes()
-    elif args.type == "paxa":
-        if args.own:
-            print_paxa(args.own)
-        else:
-            test_paxa()
-    elif args.type == "vikaraNa":
-        test_vikaraNa()
-    elif args.type == "prakriyA":
-        test_prakriyA()
+            own_func, test_func = function_mapping[args.type]
+            if args.own and own_func:
+                own_func(args.own)
+            else:
+                test_func()
 
 
 if __name__ == "__main__":
